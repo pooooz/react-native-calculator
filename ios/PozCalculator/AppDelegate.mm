2975d27c30df07ff5b61aef4fb51bcb01dd31597
Developer <developer@macbooks-MacBook-Pro.local>
2022-11-21 22:32:39 +0300

    Feat: add splash screen on IOS

--- ios/PozCalculator/AppDelegate.mm
+++ ios/PozCalculator/AppDelegate.mm
@@ -6,6 +6,8 @@
 
 #import <React/RCTAppSetupUtils.h>
 
+#import "RNSplashScreen.h"
+
 #if RCT_NEW_ARCH_ENABLED
 #import <React/CoreModulesPlugins.h>
 #import <React/RCTCxxBridgeDelegate.h>
@@ -44,7 +46,7 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 #endif
 
   NSDictionary *initProps = [self prepareInitialProps];
-  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"reactNativeCalculator", initProps);
+  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"PozCalculator", initProps);
 
   if (@available(iOS 13.0, *)) {
     rootView.backgroundColor = [UIColor systemBackgroundColor];
@@ -57,6 +59,7 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  [RNSplashScreen show];
   return YES;
 }
 
